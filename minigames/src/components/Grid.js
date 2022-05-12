e542fe50fecb9f9de5ef15e93e897cb8227a62ac
Pauhl <paul.hartmann@student.htldornbirn.at>
2022-05-12 08:01:48 +0200

    new pictures and link

--- minigames/src/components/Grid.js
+++ minigames/src/components/Grid.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Component } from "react/cjs/react.production.min";
 import styles from "./Grid.module.css";
 import Game from './Game.js';
+import lol from './lol.png'
 
 class Grid extends Component {
 
@@ -24,7 +25,7 @@ class Grid extends Component {
                 
                 <Game gameurl="footballgame" gamename="footballgame" url="https://www.laola1.at/images/redaktion/images/Fussball/International/Spanien/Barca/Ronaldo-Messi-Montage_fc3cb_f_1280x720.jpg"/>
                 <Game gameurl="flaggame" gamename="flaggame" url="https://contentlounge-s3.bf-contentlounge-production.aws.bfops.io/2018-11/austria-3045568_1920.jpg"/>
-                <Game gameurl="celebgame" gamename="Celebrity-Networth-Game" url="https://images.forbes.com/Celeb100-Thumbnail-HomepageImage-v1.png"/>
+                <Game gameurl="celebgame" gamename="Celebrity-Networth-Game" url={lol}/>
                 <Game gameurl="nix" gamename="new Games coming soon!" url="https://wallpaperaccess.com/full/2588754.jpg"/>
                 
             </div>
